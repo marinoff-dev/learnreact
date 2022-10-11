34fcb5d5a23a59cd80e2e50da7c1d12b81592b8a
Marina-Dev <offrinmarina343@gmail.com>
2022-10-11 22:34:57 +0100

    Creation of pages

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum perspiciatis autem iusto aperiam nemo amet neque cupiditate libero harum voluptates ducimus blanditiis, labore, laboriosam dolorem nisi mollitia totam eum quae nesciunt officia eos aspernatur, tenetur magnam? Reiciendis, molestiae. Assumenda, animi quam?
+                Ullam, quae consectetur perferendis accusamus corporis sunt fugiat, nesciunt deserunt molestiae incidunt exercitationem quibusdam magni ab hic error modi, autem delectus voluptatem! Aliquam exercitationem excepturi quia similique adipisci consectetur molestias possimus facilis labore.
+                Nihil voluptatibus nobis nostrum deleniti asperiores quaerat. Provident voluptatem eaque iure ab magni itaque aperiam possimus, praesentium sunt! Ut fuga ex enim, quod asperiores esse eum.
+            </p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates pariatur deserunt quaerat nostrum numquam, laboriosam assumenda provident illum modi facilis iste error! Blanditiis illum nobis repellat laudantium tempora voluptatibus, doloremque, aliquam animi maxime officiis error explicabo, beatae amet optio esse laboriosam aspernatur vero rerum? 
+                Porro libero tenetur fugit, magnam cumque consequuntur reprehenderit! Odit id architecto voluptate voluptatibus, ad deserunt culpa, ducimus sunt voluptates doloremque dolore, nostrum facere? Autem quo, esse ea quaerat hic molestias. Alias aliquid quos blanditiis facilis possimus laborum quo molestias a.
+                Minima fugiat numquam enim saepe qui voluptas labore consectetur fugit eum voluptatum, error quidem. Quasi vero doloremque deleniti consequatur rerum delectus, totam blanditiis pariatur eaque iure aperiam tempora odio quia veritatis deserunt hic porro modi dolorem facere enim debitis? Quas, perferendis. Necessitatibus, minima eius at laudantium illum laboriosam ad vero vitae.
+                Accusantium, animi error repellendus quae maiores vero minus doloribus quis pariatur aspernatur sed molestias ea? Ad neque, voluptate nihil quae dolor exercitationem, aut consequatur voluptatibus reiciendis eligendi perspiciatis iste laudantium laborum odio quasi facilis earum saepe provident reprehenderit id quod praesentium laboriosam?
+                 Nam, reiciendis nisi possimus ex blanditiis ipsa quae nobis repellat officiis similique dignissimos iusto dicta quod doloremque. Quibusdam ratione ut rem mollitia laboriosam.
+            </p>
+        </div>
+    );
+};
+
+export default About;
